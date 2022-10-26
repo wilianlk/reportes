2715a31b599f377a83db88696da224508a0ce2b1
wilianlk <wilian.andres@hotmail.es>
2022-10-26 10:17:19 -0500

    Se crea consulta para reporte juego mistery

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -14,65 +14,57 @@ class ReportsController extends Controller
 //     */
     public function index()
     {
-        // se validara git
-        $users = DB::select("SELECT c.id_cliente,
-        concat(IFNULL(c.nombre,''),' ',
-        IFNULL(c.apellido,'')) AS nombre_cliente,
-        if(c.mayorista,'CALLCENTER','SUCURSAL') AS origen,
-        c.origen_ficha,
-         IFNULL(gi.nombre,'NO DEFINIDO') AS idioma,
-         ct.nombre AS tipo_cliente,
-         tc.nombre AS estado_compra,
-         z.zip_code,
-         ci.ciudad ,
-         ci.estado AS pais_estado,
-         ci.pais AS pais,g.nombre_grupo AS grupo_ventas,
-         g.nombre_grupo AS filtro_grupo_ventas,
-         if(c.mayorista,'CALLCENTER','SUCURSAL') AS origen,
-         if(c.mayorista,'CALLCENTER','SUCURSAL') AS filtro_origen,
-         c.origen_ficha AS filtro_origen_ficha,
-         ci.estado AS filtro_pais_estado,
-         ci.pais AS filtro_pais,
-         ct.nombre AS filtro_tipo_cliente,
-          YEAR(c.created_at) AS anio,
-           MONTH(c.created_at) AS mes,
-             WEEK(c.created_at) AS semana,
-             i.tipo AS tipo_telefono,
-             i.numero AS numero_telefono,
-             i.extension,
-             i.autoriza_sms,
-             i.email,
-             1 AS cantidad,
-             cmc.nombre AS medio_conocimiento,
-                           CONCAT(sc.first_name,' ',sc.last_name) creado_por
-        FROM cliente c
-        INNER JOIN sf_guard_user sc ON sc.id = c.created_by
-        LEFT JOIN cli_grupo_ventas cg ON c.id_cliente=cg.id_cliente
-        LEFT JOIN grupo_ventas g ON cg.id_grupo_ventas=g.id_grupo_ventas
-        LEFT JOIN cli_medio_conocimiento cmc ON c.id_medio_conocimiento = cmc.id_medio_conocimiento
-        LEFT JOIN z_ciudad_prim_cli z ON c.id_cliente=z.id_cliente
-        LEFT JOIN Z_ciudad_estado_pais ci ON z.id_ciudad=ci.id_ciudad
-        LEFT JOIN cli_tipo ct ON c.id_tipo_cliente=ct.id_tipo_cliente
-        LEFT JOIN gen_tipo_compra tc ON c.id_tipo_compra=tc.id_tipo_compra
-        LEFT JOIN gen_idioma gi ON c.id_idioma=gi.id_idioma
-        LEFT JOIN (
-        SELECT c1.id_cliente,t.tipo,t.numero,t.extension,if(t.autoriza_sms,'SI','NO')autoriza_sms,c1.email
-        FROM cliente c1
-        INNER JOIN cli_telefono t ON c1.id_cliente=t.id_cliente
-        GROUP BY c1.id_cliente UNION
-        SELECT cc.id_cliente,ctl.tipo,ctl.numero,ctl.extension,if(ctl.autoriza_sms,'SI','NO')autoriza_sms,cc.email
-        FROM cli_contacto cc
-        INNER JOIN cli_telefono_contacto ctl ON cc.id_cliente_contacto=ctl.id_cliente_contacto
-        GROUP BY cc.id_cliente)i ON c.id_cliente=i.id_cliente
-        WHERE (c.mayorista=1 AND c.origen_ficha IS NULL) OR (c.mayorista=1 AND c.origen_ficha='USA') OR (cg.id_grupo_ventas IS NOT NULL AND (c.mayorista=1 OR c.mayorista=0))
-        AND c.id_status <> 0
-        GROUP BY c.id_cliente");
+        $users = DB::select("
+                            SELECT cs.id_cliente,IF(IFNULL(cs.cliente_exist,2) = 1,'Cliente Existente','Cliente No Existente') creacion_cliente,CONCAT(c.nombre,' ',c.apellido) name_client,IFNULL(ct.nombre,ct2.nombre) tipo_cliente,cs.telefono,cs.email,
+                            cs.aplicacion,t.nombre tiendas,
+                            DAY(cs.created_at) dia_numero,DAYNAME(cs.created_at) dia_nombre_eng,
+                            CASE DAYOFWEEK(cs.created_at)
+                                WHEN 1 THEN 'Domingo'
+                                WHEN 2 THEN 'Lunes'
+                                WHEN 3 THEN 'Martes'
+                                WHEN 4 THEN 'Miércoles'
+                                WHEN 5 THEN 'Jueves'
+                                WHEN 6 THEN 'Viernes'
+                                WHEN 7 THEN 'Sábado'
+                            END nombre_dia_es,DAYOFWEEK(cs.created_at) dia_semana,WEEKOFYEAR(cs.created_at) numero_semana,
+                            MONTH(cs.created_at) mes,
+                            CASE MONTH(cs.created_at)
+                                WHEN 1 THEN 'Enero'
+                                WHEN 2 THEN  'Febrero'
+                                WHEN 3 THEN 'Marzo'
+                                WHEN 4 THEN 'Abril'
+                                WHEN 5 THEN 'Mayo'
+                                WHEN 6 THEN 'Junio'
+                                WHEN 7 THEN 'Julio'
+                                WHEN 8 THEN 'Agosto'
+                                WHEN 9 THEN 'Septiembre'
+                                WHEN 10 THEN 'Octubre'
+                                WHEN 11 THEN 'Noviembre'
+                                WHEN 12 THEN 'Diciembre'
+                            END mes_nombre,YEAR(cs.created_at) ano,
+                            CONCAT(s.first_name,' ',s.last_name) quien_envio,ud.nombre departamento_pertenece,
+                            cs.voucher,cs.descuento,cs.uso,cs.codigo
+                            FROM cli_juego_scrach_configuracion cs
+                            LEFT JOIN cliente c ON c.id_cliente = cs.id_cliente
+                            LEFT JOIN cli_tipo ct ON ct.id_tipo_cliente = cs.tipo_cliente
+                            LEFT JOIN cli_tipo ct2 ON ct2.id_tipo_cliente = c.id_tipo_cliente
+                            LEFT JOIN tienda t ON t.id_tienda = cs.id_tienda_juego
+                            LEFT JOIN sf_guard_user s ON s.id = cs.id_user
+                            LEFT JOIN user_departamento ud ON ud.id_user_departamento = s.id_user_departamento
+                            WHERE 1
+                            ORDER BY
+                            (CASE
+                                WHEN cs.aplicacion = 'callcenterusa' THEN 1
+                                WHEN cs.aplicacion = 'sucursales' THEN 2
+                            END) ASC");
+
         return response()->json($users);
     }
- public function testfelipe()
+
+    public function testfelipef()
     {
-        
- }
+
+    }
 
     /**
      * Show the form for creating a new resource.
@@ -87,7 +79,7 @@ class ReportsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -98,7 +90,7 @@ class ReportsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -109,7 +101,7 @@ class ReportsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -120,8 +112,8 @@ class ReportsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -132,7 +124,7 @@ class ReportsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
